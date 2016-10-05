bd8599b0f1230d1c74b8f651ab15aea58a416076
Praneeta Jhanwar <praneeta@ccs.neu.edu>
2016-10-05 15:09:37 -0700

    Adding solutions

--- MeetingRooms.java
+++ MeetingRooms.java
@@ -19,20 +19,17 @@ return false.
  *     Interval(int s, int e) { start = s; end = e; }
  * }
  */
-
-public class MeetingRooms {
-	public boolean canAttendMeetings(Interval[] intervals) {
-        Arrays.sort(intervals, new IntervalComparator());
-        for (int i = 0; i < intervals.length-1; i++) {
-            if (intervals[i].end > intervals[i+1].start)
+public class Solution {
+    public boolean canAttendMeetings(Interval[] intervals) {
+        Arrays.sort(intervals, new Comparator<Interval>() {
+            public int compare(Interval a, Interval b) {
+                return a.start - b.start;
+            }
+        });
+        for (int i = 0; i < intervals.length - 1; i++) {
+            if (intervals[i].end > intervals[i + 1].start)
                 return false;
         }
         return true;
-    }
-}
-
-class IntervalComparator implements Comparator<Interval> {
-    public int compare (Interval i1, Interval i2) {
-        return (i1.start - i2.start);
     }
 }
