1a4454df40f1cca610a1959a1fa82e71241a9c38
Praneeta Jhanwar <praneeta@ccs.neu.edu>
2016-10-21 00:14:30 -0700

    Adding solutions

--- WallsAndGates.java
+++ WallsAndGates.java
@@ -21,31 +21,31 @@ After running your function, the 2D grid should be:
   0  -1   3   4
   
   */
-public class WallsAndGates {
-	public void wallsAndGates(int[][] rooms) {
+public class Solution {
+    public void wallsAndGates(int[][] rooms) {
         if (rooms == null || rooms.length == 0)
             return;
         for (int i = 0; i < rooms.length; i++) {
             for (int j = 0; j < rooms[0].length; j++) {
                 if (rooms[i][j] == 0) {
-                    bfs(rooms, i+1, j, rooms[i][j]+1);
-                    bfs(rooms, i-1, j, rooms[i][j]+1);
-                    bfs(rooms, i, j+1, rooms[i][j]+1);
-                    bfs(rooms, i, j-1, rooms[i][j]+1);
+                    bfs(rooms, i+1, j, 1);
+                    bfs(rooms, i-1, j, 1);
+                    bfs(rooms, i, j+1, 1);
+                    bfs(rooms, i, j-1, 1);
                 }
             }
         }
     }
     
-    public void bfs(int[][] rooms, int i, int j, int dis) {
-        if (i<0 || j<0 || i>=rooms.length || j>=rooms[0].length || rooms[i][j] == -1 || rooms[i][j] == 0)
+    public void bfs(int[][] rooms, int i, int j, int dist) {
+        if (i < 0 || j < 0 || i >= rooms.length || j >= rooms[0].length || rooms[i][j] == -1 || rooms[i][j] == 0)
             return;
-        if (rooms[i][j] <= dis)
+        if (rooms[i][j] <= dist)
             return;
-        rooms[i][j] = dis;
-        bfs(rooms, i+1, j, rooms[i][j]+1);
-        bfs(rooms, i-1, j, rooms[i][j]+1);
-        bfs(rooms, i, j+1, rooms[i][j]+1);
-        bfs(rooms, i, j-1, rooms[i][j]+1);
-    } 
+        rooms[i][j] = dist;
+        bfs(rooms, i+1, j, dist + 1);
+        bfs(rooms, i-1, j, dist + 1);
+        bfs(rooms, i, j+1, dist + 1);
+        bfs(rooms, i, j-1, dist + 1);
+    }
 }
